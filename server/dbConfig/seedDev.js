68eeaa177838b27776d56af22ac6ea4aa300f9a5
Parth Patel <parth6796@yahoo.com>
2021-09-05 13:15:41 +0100

    moved seed file

--- server/dbConfig/seedDev.js
+++ server/dbConfig/seedDev.js
@@ -1,6 +1,6 @@
 const db = require('./init');
 const fs = require('fs');
 
-const seeds = fs.readFileSync(__dirname + '/dev_seeds.sql').toString();
+const seeds = fs.readFileSync(__dirname + '../../db/2_seed.sql').toString();
 
 db.query(seeds, () => console.log('Dev database seeded'));
