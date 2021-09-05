f1b0b1ae3d591000208bc38f0e9c2beec97b6ba3
Parth Patel <parth6796@yahoo.com>
2021-09-05 12:23:48 +0100

    trying to get front end to fetch auth details

---
+++ client/js/auth.js
@@ -0,0 +1,30 @@
+const { options } = require("../../server/server");
+
+const url = window.location.hostname.includes('localhost')
+	? 'http://localhost:3000'
+	: 'another url if I want to deploy';
+
+async function requestLogin(e){
+    e.preventDefault();
+    try {
+        const options = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: e.target.username.value,
+                password_digest: e.target.password.value
+            })
+        };
+        const r = await fetch(url, options);
+        const data = await r.json();
+        console.log("data: "+ data);
+        if(!data.success) {
+            throw new Error("Login not authorised")
+        }
+        login(data.token);
+    } catch (error) {
+        console.warn(error);
+    }
+}
