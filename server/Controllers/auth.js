9f3de35509a973d762d778da04ae25eef8f605f0
Parth Patel <parth6796@yahoo.com>
2021-09-05 10:50:52 +0100

    added login and register routers

--- server/Controllers/auth.js
+++ server/Controllers/auth.js
@@ -6,3 +6,48 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const User = require("../Models/user");
+
+router.post("/register", async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashed = await bcrypt.hash(req.body.password, salt)
+        await User.create({...req.body, password: hashed})
+        res.status(201)
+        .json({msg: 'User created'})
+    } catch (error) {
+        res.status(500)
+        .json({error})
+    }
+})
+
+router.post("/login", async(req, res) => {
+    try {
+        const user = await User.findByUsername(req.body.username);
+        if(!user){
+            throw new Error("No user with this username");
+        }
+        const authed = await bcrypt.compare(req.body.password, user.passwordDigest);
+        if(!!authed){
+            const payload = {
+                username: user.username,
+                email: user.email
+            };
+            const sendToken = (error, token) => {
+                if(error){
+                    throw new Error("Error in token generation");
+                }
+                res.status(200)
+                .json({success: true,
+                        token: "Bearer "+token});
+            }
+            jwt.sign(payload, process.env.SECRET, sendToken);
+        }else{
+            throw new Error("User could not be authenticated");
+        }
+    } catch (error) {
+        res.status(401)
+        .json({error});
+    }
+})
+
+module.exports = router;
