a497afd2e3c219ea5e42409abaad4ee6f409ebcf
Parth Patel <parth6796@yahoo.com>
2021-09-05 10:37:30 +0100

    added verify token function in middleware

---
+++ server/Middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+
+const verifyToken = (req, res, next) => {
+    const header = req.headers["authorization"];
+    if(header) {
+        const token = header.split(" ")[1];
+        jwt.verify(token, process.env.SECRET, async (err, data) => {
+            if(err){
+                res.status(403)
+                .json({err: "Invalid Token"});
+            }else{
+                next();
+            }
+        });
+    }else{
+        res.status(403)
+        .json({err: "Missing Token"});
+    }
+}
+
+module.exports = verifyToken
