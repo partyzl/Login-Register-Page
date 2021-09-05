955f813416aa5c06039895805ee1dcc897810e3d
Parth Patel <parth6796@yahoo.com>
2021-09-05 13:22:58 +0100

    removing uneccessary content

--- server/dbConfig/dev_seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, email, password_digest) 
-VALUES
-    ('Partyzl', 'party@parth.co.uk', 'tu9ibtoi4tbh2hhuet' ),
-    ('James', 'james@james.com', 'h4hoeuba3r3tbaeu' ),
-    ('Kyle', 'kyle@kyle.dk', 'ahudnthpdohbapbb243' );
