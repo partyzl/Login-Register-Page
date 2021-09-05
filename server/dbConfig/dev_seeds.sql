75800aabe73febfda60ce762af65c5929e6d2eb0
Parth Patel <parth6796@yahoo.com>
2021-09-05 10:10:18 +0100

    seeded sql file

---
+++ server/dbConfig/dev_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, email, password_digest) 
+VALUES
+('James', 'james@james.co.uk', 'password'),
+('Kyle', 'kyle@kyle.com', 'password'),
+('Grant', 'grant@grant.com', 'password');
