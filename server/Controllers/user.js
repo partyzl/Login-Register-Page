ba2c4d43b9bd6ac70945243f6b048ba91f37df87
Parth Patel <parth6796@yahoo.com>
2021-09-05 11:11:38 +0100

    added get by username controller

--- server/Controllers/user.js
+++ server/Controllers/user.js
@@ -5,8 +5,26 @@ const User = require("../Models/user");
 const { verifyToken } = require('../Middleware/auth');
 
 router.get('/', verifyToken, async (req, res) => {
-    const users = await User.all
-    res.json(users)
+    try{
+        const users = await User.all
+        res.json(users)
+    }catch(error){
+        console.error("Could not retrieve users");
+        res.status(500)
+        .json({error})
+    }
+})
+
+router.get('/:username', verifyToken, async (req, res) => {
+    try{
+        const user = await User.findByUsername(req.params.username);
+        res.status(200)
+        .json(user)
+    }catch(error){
+        console.error("Could not find user");
+        res.status(404)
+        .json({error})
+    }
 })
 
 module.exports = router;
